1f8810681b44824fd2bb31c4f9b24e8ea660ee0e
Ben <torr3kuo@gmail.com>
2022-01-04 23:02:04 +0800

    add nav homedetail

--- app/src/main/java/com/example/kotindemo/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/example/kotindemo/ui/slideshow/SlideshowFragment.kt
@@ -8,7 +8,6 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.kotindemo.R
 import com.example.kotindemo.databinding.FragmentSlideshowBinding
 
 class SlideshowFragment : Fragment() {
