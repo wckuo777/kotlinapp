b54998a77c6b26fa2d9b043ae8f4652c49878241
wckuo777 <goho3kuo@gmail.com>
2022-01-17 17:35:36 +0800

    add webview

--- app/src/main/java/com/example/kotlindemo/ui/friendwebsites/FriendWebSitesFragment.kt
+++ app/src/main/java/com/example/kotlindemo/ui/friendwebsites/FriendWebSitesFragment.kt
@@ -6,9 +6,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
+
 import com.example.kotlindemo.R
+import com.example.kotlindemo.databinding.FriendWebSitesFragmentBinding
+import android.webkit.WebSettings
+import android.webkit.WebViewClient
+
+
+
+
+
+
 
 class FriendWebSitesFragment : Fragment() {
+    private var _binding: FriendWebSitesFragmentBinding ?= null
+    private val binding get() = _binding!!
 
     companion object {
         fun newInstance() = FriendWebSitesFragment()
@@ -20,7 +33,15 @@ class FriendWebSitesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.friend_web_sites_fragment, container, false)
+        _binding = FriendWebSitesFragmentBinding.inflate(inflater, container, false)
+        val mWebView = _binding!!.mywebview
+        mWebView.loadUrl("https://www.google.com/")
+        val webSettings = mWebView.settings
+        webSettings.javaScriptEnabled = true
+        mWebView.webViewClient = WebViewClient()
+
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
