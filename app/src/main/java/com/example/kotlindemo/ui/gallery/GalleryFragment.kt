0745e43ca88f9d7c0ac43e5fc89d947ad8acd22a
wckuo777 <goho3kuo@gmail.com>
2022-01-25 13:09:44 +0800

    add basic spinner

--- app/src/main/java/com/example/kotlindemo/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/kotlindemo/ui/gallery/GalleryFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -12,6 +13,10 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.kotlindemo.R
 import com.example.kotlindemo.databinding.FragmentGalleryBinding
 import com.example.kotlindemo.ui.home.HomeViewModel
+import android.widget.ArrayAdapter
+
+
+
 
 
 class GalleryFragment : Fragment() {
@@ -38,6 +43,17 @@ class GalleryFragment : Fragment() {
 
         val textView: TextView = binding.textGallery
         val textView2: TextView = binding.textView2
+        val spinner: Spinner = binding.spinner
+
+        val arrayList1 = ArrayList<String>()
+        arrayList1.add("Bangalore");
+        arrayList1.add("Delhi");
+        arrayList1.add("Mumbai");
+        val adp: ArrayAdapter<String> = ArrayAdapter<String>(requireContext(), android.R.layout.simple_spinner_dropdown_item, arrayList1)
+        spinner.adapter = adp
+
+
+
         galleryViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
             textView2.text = it
