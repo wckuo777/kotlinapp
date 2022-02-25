f825c6763611485c9732772e63a5eb8f7a4ccaeb
wckuo777 <goho3kuo@gmail.com>
2022-02-25 17:22:00 +0800

    persist data to add function

---
+++ app/src/main/java/com/example/kotlindemo/ui/slideshow/AddItemFragment.kt
@@ -0,0 +1,138 @@
+package com.example.kotlindemo.ui.slideshow
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.kotlindemo.InventoryApplication
+import com.example.kotlindemo.InventoryViewModel
+import com.example.kotlindemo.InventoryViewModelFactory
+import com.example.kotlindemo.data.Item
+import com.example.kotlindemo.databinding.FragmentAddItemBinding
+
+
+class AddItemFragment: Fragment() {
+
+    // Use the 'by activityViewModels()' Kotlin property delegate from the fragment-ktx artifact
+    // to share the ViewModel across fragments.
+    private val viewModel: InventoryViewModel by activityViewModels {
+        InventoryViewModelFactory(
+            (activity?.application as InventoryApplication).database
+                .itemDao()
+        )
+    }
+    private val navigationArgs: ItemDetailFragmentArgs by navArgs()
+
+    lateinit var item: Item
+
+    // Binding object instance corresponding to the fragment_add_item.xml layout
+    // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
+    // when the view hierarchy is attached to the fragment
+    private var _binding: FragmentAddItemBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAddItemBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    /**
+     * Returns true if the EditTexts are not empty
+     */
+    private fun isEntryValid(): Boolean {
+        return viewModel.isEntryValid(
+            binding.itemName.text.toString(),
+            binding.itemPrice.text.toString(),
+            binding.itemCount.text.toString(),
+        )
+    }
+
+    /**
+     * Binds views with the passed in [item] information.
+     */
+    private fun bind(item: Item) {
+        val price = "%.2f".format(item.itemPrice)
+        binding.apply {
+            itemName.setText(item.itemName, TextView.BufferType.SPANNABLE)
+            itemPrice.setText(price, TextView.BufferType.SPANNABLE)
+            itemCount.setText(item.quantityInStock.toString(), TextView.BufferType.SPANNABLE)
+            saveAction.setOnClickListener { updateItem() }
+        }
+    }
+
+    /**
+     * Inserts the new Item into database and navigates up to list fragment.
+     */
+    private fun addNewItem() {
+        if (isEntryValid()) {
+            viewModel.addNewItem(
+                binding.itemName.text.toString(),
+                binding.itemPrice.text.toString(),
+                binding.itemCount.text.toString(),
+            )
+            val action = AddItemFragmentDirections.actionAddItemFragmentToNavSlideshow()
+            findNavController().navigate(action)
+        }
+    }
+
+    /**
+     * Updates an existing Item in the database and navigates up to list fragment.
+     */
+    private fun updateItem() {
+        if (isEntryValid()) {
+            viewModel.updateItem(
+                this.navigationArgs.itemId,
+                this.binding.itemName.text.toString(),
+                this.binding.itemPrice.text.toString(),
+                this.binding.itemCount.text.toString()
+            )
+            val action = AddItemFragmentDirections.actionAddItemFragmentToNavSlideshow()
+            findNavController().navigate(action)
+        }
+    }
+
+    /**
+     * Called when the view is created.
+     * The itemId Navigation argument determines the edit item  or add new item.
+     * If the itemId is positive, this method retrieves the information from the database and
+     * allows the user to update it.
+     */
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val id = navigationArgs.itemId
+        if (id > 0) {
+            viewModel.retrieveItem(id).observe(this.viewLifecycleOwner) { selectedItem ->
+                item = selectedItem
+                bind(item)
+            }
+        } else {
+            binding.saveAction.setOnClickListener {
+                addNewItem()
+            }
+        }
+    }
+
+    /**
+     * Called before fragment is destroyed.
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // Hide keyboard.
+        val inputMethodManager = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as
+                InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(requireActivity().currentFocus?.windowToken, 0)
+        _binding = null
+    }
+}
